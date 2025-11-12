df57933c2b15f6ebc34f1a08ea2b586e984f5a95
Sudhanshu Kumar <160045794+sudhanshugks@users.noreply.github.com>
2025-11-12 15:16:37 +0530

    Create App.js

---
+++ Experiment_9.1/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: 40, fontFamily: "sans-serif" }}>
+      <h1>🚀 React + Multi-Stage Docker</h1>
+      <p>Your app is running inside an optimized Docker container with Nginx.</p>
+    </div>
+  );
+}
